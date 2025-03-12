f2405dd6c6ab2f0c06344988dbbec235b62de79e
Barg0s <150375261+Barg0s@users.noreply.github.com>
2025-03-12 18:40:31 +0100

    deures1

--- Deures03/src/main/java/com/exercici0305/Competidor.java
+++
@@ -1,5 +0,0 @@
-package com.exercici0305;
-
-public interface Competidor {
-    void competir();
-}
--- Deures03/src/main/java/com/exercici0305/Esportista.java
+++
@@ -1,6 +0,0 @@
-package com.exercici0305;
-
-public interface Esportista {
-
-    void entrenar();
-}
